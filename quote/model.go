5b4a03eff9a04699331e37a32aed1bec2d673858
Neal-C <pop6d531@duck.com>
2023-11-25 15:52:54 +0100

    feat(QUOTE): setup quote

---
+++ quote/model.go
@@ -0,0 +1,14 @@
+package quote;
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Quote struct {
+	Id         uuid.UUID `json:"id,omitempty" db:"id"`
+	Book       string    `json:"book,omitempty" db:"book"`
+	Quote      string    `json:"quote,omitempty" db:"quote"`
+	CreatedAt  time.Time `json:"insertedAt,omitempty" db:"created_at"`
+	UpdatedAt  time.Time `json:"updatedAt,omitempty" db:"updated_at"`
+}
