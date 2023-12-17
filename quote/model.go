786a672bc22c56c0b5614523ea7b86d9c07f1fcd
Neal-C <pop6d531@duck.com>
2023-12-17 17:21:04 +0100

    feat(QUOTE::Model): impl'd struct to GRPC conversion

--- quote/model.go
+++ quote/model.go
@@ -1,8 +1,11 @@
-package quote;
+package quote
 
 import (
 	"time"
+
+	"github.com/Neal-C/go-grpc-crud/protocodegen"
 	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Quote struct {
@@ -11,4 +14,15 @@ type Quote struct {
 	Quote      string    `json:"quote,omitempty" db:"quote"`
 	CreatedAt  time.Time `json:"insertedAt,omitempty" db:"created_at"`
 	UpdatedAt  time.Time `json:"updatedAt,omitempty" db:"updated_at"`
+}
+
+func QuoteToGRPCQuote(quote Quote) protocodegen.Quote {
+	return protocodegen.Quote{
+		// investigate this
+		Id: quote.ID.URN(),
+		Book: quote.Book,
+		Quote: quote.Quote,
+		CreatedAt:timestamppb.New(quote.CreatedAt),
+		UpdatedAt: timestamppb.New(quote.UpdatedAt),
+	}
 }
