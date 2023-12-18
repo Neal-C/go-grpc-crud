705e4f0d61b2bd6838254333042d099cc782ec39
Neal-C <pop6d531@duck.com>
2023-12-18 19:28:38 +0100

    feat(MIGRATIONS): syntax errors fixed

--- migrations/01_schema.ddl.up.sql
+++ migrations/01_schema.ddl.up.sql
@@ -1,10 +1,10 @@
 SET TIMEZONE TO 'Europe/Prague';
 
 CREATE TABLE IF NOT EXISTS quote (
-    id UUID PRIMARY KEY
-    book VARCHAR(63)
-    quote VARCHAR(255)
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    id UUID PRIMARY KEY,
+    book VARCHAR(63),
+    quote VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     UNIQUE(book, quote)
 );
